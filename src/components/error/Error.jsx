7ff877bf76f904477899104b5e0edfd5e3669d7e
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2023-05-16 12:24:02 +0200

    handle page error

---
+++ src/components/error/Error.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import './_error.scss';
+
+
+export default function ErrorPage() {
+	return (
+		<div className="error">
+			<p className="error__number">404</p>
+			<p className="error__text">Oups! La page que vous demandez n'existe pas.</p>
+			<Link to="/" className="error__link">
+				Retourner sur la page d’accueil
+			</Link>
+		</div>
+	);
+}
--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,16 +1,13 @@
-import { useRouteError } from "react-router-dom";
+import ErrorPage from "../../components/error/Error";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
 
-export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
-
-  return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-  );
+export default function Error() {
+	return (
+		<>
+      <Header />
+			<ErrorPage />
+      <Footer />
+		</>
+	);
 }
