53f3a2da510f7aec795d744a58a6402f3f3ad00a
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2023-05-20 10:47:39 +0200

    finalise carrousel

--- src/components/carrousel/Carrousel.jsx
+++ src/components/carrousel/Carrousel.jsx
@@ -4,49 +4,45 @@ import ArrowLeft from '../../assets/arrow_carousel_left.png'
 import { useState } from 'react'
 
 
-export default function Carrousel ({ slide }) {
+export default function Slide({ imageSlide }) {
 
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const length = slide.length;
+    // Creation of a state variable currentIndex which represents the index of the image currently displayed in the carrousel
+    // and a function that updates the variable 
+    const [currentIndex, setCurrentIndex] = useState(0) 
 
     const nextSlide = () => {
         setCurrentIndex(currentIndex + 1)
-        if (currentIndex === length - 1) {
+        if(currentIndex === imageSlide.length - 1)
             setCurrentIndex(0)
-        }
     }
 
     const prevSlide = () => {
         setCurrentIndex(currentIndex - 1)
-        if (currentIndex === 0) {
-            setCurrentIndex(length - 1)
-        }
+        if(currentIndex === 0)
+            setCurrentIndex(imageSlide.length - 1)
     }
 
     return (
         <div className='carrousel'>
+            <img src={ imageSlide[currentIndex] } alt="carrousel display" className='carrousel__img'/>
 
-            { length > 1 &&
-                <>
-                    <img
-                        src={ ArrowLeft }
-                        alt="gauche"
+            { imageSlide.length > 1 && 
+                <div>
+                    <img 
+                        className='carrousel__arrowRight' 
+                        src={ ArrowRight } 
+                        alt="show next slide" 
+                        onClick={ nextSlide  }
+                    />
+                    <img 
+                        className='carrousel__arrowLeft' 
+                        src={ ArrowLeft }  
+                        alt="show previous slide" 
                         onClick={ prevSlide }
-                        className='carrousel__arrowLeft'
                     />
-
-                    <img
-                        src={ ArrowRight }
-                        alt="droite"
-                        onClick={ nextSlide }
-                        className='carrousel__arrowRigth'
-                    />
-
-                    <p className='caroussel_number'>{ currentIndex + 1 } / { slide.length }</p>
-                </>
-            }
-
+                    <p className='carrousel__number'>{ currentIndex + 1 } / { imageSlide.length }</p>
+                </div>
+            } 
         </div>
     )
-
 }
--- src/pages/housing/Housing.jsx
+++ src/pages/housing/Housing.jsx
@@ -1,34 +1,27 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
-import data from '../../data/logements.json'
+import datas from '../../data/logements.json'
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 import Carrousel from '../../components/carrousel/Carrousel';
 
 
-export default function Housing() {
+export default function Accomodation() {
+	
+	const [imageSlide, setImageSlide] = useState([]);
 
-    const [slide, setSlide] = useState([])
+	const idAccomodation = useParams('id').id;
+	
+	useEffect(() => {
+		const dataCurrentAccomodation = datas.filter(data => data.id === idAccomodation);
+		setImageSlide(dataCurrentAccomodation[0].pictures);
+	}, [idAccomodation]);
 
-    const idHousing = useParams('id').id
-    const dataHousing = data.filter(data => data.id === idHousing)
-
-    useEffect(() => {
-		const dataHousing = data.filter(data => data.id === idHousing);
-		setSlide(dataHousing[0].pictures);
-	}, [idHousing]);
-
-    // const name = dataHousing[0].host.name.split(' ');
-    // const rating = dataHousing[0].rating;
-    // const description = dataHousing[0].description;
-    // const equipments = dataHousing[0].equipments;
-
-
-    return (
-        <div>
-            <Header />
-                <Carrousel slide={ slide }/>
-            <Footer />
-        </div>
-    )
+	return (
+		<>
+			<Header/>
+			<Carrousel imageSlide={imageSlide}/>		
+			<Footer/>
+		</>
+	)
 }
