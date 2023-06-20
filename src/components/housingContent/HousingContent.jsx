432c788621edab75b4313a9cd218a29342cd493a
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2023-06-20 12:21:09 +0200

    add handle error about id housing

--- src/components/housingContent/HousingContent.jsx
+++ src/components/housingContent/HousingContent.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import datas from "../../data/logements.json";
 import Carrousel from "../../components/carrousel/Carrousel";
 import Collapse from "../../components/collapse/Collapse";
@@ -7,60 +7,68 @@ import emptyStar from "../../assets/emptyStar.png";
 import fullStar from "../../assets/fullStar.png";
 import './_housingContent.scss';
 
-
 export default function HousingContent() {
   const [imageSlide, setImageSlide] = useState([]);
-  const idHousing = useParams().id;
-  const dataCurrentHousing = datas.filter((data) => data.id === idHousing);
+  const { id } = useParams();
+  const dataCurrentHousing = datas.find((data) => data.id === id);
 
   useEffect(() => {
-    const dataCurrentHousing = datas.filter((data) => data.id === idHousing);
-    setImageSlide(dataCurrentHousing[0].pictures);
-  }, [idHousing]);
+    const dataCurrentHousing = datas.find((data) => data.id === id);
+    if (dataCurrentHousing) {
+      setImageSlide(dataCurrentHousing.pictures);
+    }
+  }, [id]);
+
+  if (!dataCurrentHousing) {
+    return <Navigate to="/404" />;
+  }
+
+  const { title, location, tags, host, rating, description, equipments } = dataCurrentHousing;
 
   return (
     <>
-      <Carrousel imageSlide={ imageSlide } />
+      <Carrousel imageSlide={imageSlide} />
       <main>
         <div className="housing">
           <div className="housing__infos">
-            <h1 className="housing__infos__h1">{ dataCurrentHousing[0].title }</h1>
-            <p>{ dataCurrentHousing[0].location }</p>
+            <h1 className="housing__infos__h1">{title}</h1>
+            <p>{location}</p>
 
-            { dataCurrentHousing[0].tags.map((tag, index) => {
-              return <button className="housing__infos__buttons" key={ index }>{ tag }</button>;
-            }) }
+            {tags.map((tag, index) => (
+              <button className="housing__infos__buttons" key={index}>{tag}</button>
+            ))}
           </div>
 
           <div className="housing__infos2">
             <div className="housing__host">
-              <p className="housing__host__p">{ dataCurrentHousing[0].host.name }</p>
-              <img className="housing__host__img" src={ dataCurrentHousing[0].host.picture } alt="" />
+              <p className="housing__host__p">{host.name}</p>
+              <img className="housing__host__img" src={host.picture} alt="" />
             </div>
 
             <div className="housing__rate">
-              { [...Array(5)].map((star, index) => {
+              {[...Array(5)].map((_, index) => {
                 const rateValue = index + 1;
                 return (
-                  <img className="housing__rate__star"
-                    key={ index }
-                    src={ rateValue <= dataCurrentHousing[0].rating ? fullStar : emptyStar }
+                  <img
+                    className="housing__rate__star"
+                    key={index}
+                    src={rateValue <= rating ? fullStar : emptyStar}
                     alt="rate with star"
                   />
                 );
-              }) }
+              })}
             </div>
           </div>
         </div>
 
         <div className="housing__collapse">
-            <div className="housing__collapse__description">
-                <Collapse title={ "Description" } content={ dataCurrentHousing[0].description } />
-            </div>
+          <div className="housing__collapse__description">
+            <Collapse title={"Description"} content={description} />
+          </div>
 
-            <div className="housing__collapse__equipments">
-                <Collapse title={ "Equipements" } content={ dataCurrentHousing[0].equipments } />
-            </div>
+          <div className="housing__collapse__equipments">
+            <Collapse title={"Equipements"} content={equipments} />
+          </div>
         </div>
       </main>
     </>
